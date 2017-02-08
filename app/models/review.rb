7691bf25d33cef8f4f415dde61612fcf7b3f2e7f
Aurelien Burget <aurelion@gmail.com>
2017-02-08 12:59:45 +0100

    Corrected model validation and add 2 empty controllers

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,7 @@ class Review < ApplicationRecord
   # validates :rating, numericality: { only_integer: true },
   #   message: "must be a number between 0 and 5" }
 
-  validates :rating, numericality: {
-    only_integer: true,
-    greater_than_or_equal_to: 0,
-    less_than: 6 }
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: (0..5) }
   validates :content, presence: true, allow_blank: false
   validates :restaurant, presence: true
   belongs_to :restaurant
