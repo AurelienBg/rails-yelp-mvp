7864a7044efb4783275dd6d45033c42ccc51bcd2
Aurelien Burget <aurelion@gmail.com>
2017-02-08 13:19:28 +0100

    adding controllers for Restaurant and Review models

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,15 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [ :chef ]
+
+  def top
+    @restaurants = Restaurant.where(stars: 3)
+  end
+  def chef
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
